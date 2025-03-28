01fba4ae410b5333e9bea34e0dde3ed6ef05b7f6
ClementSellyPro <clementselly.pro@gmail.com>
2025-03-28 09:56:54 +0400

    profile page done - but little display bug for the TabBar

--- fitness-app-complete/app/(tabs)/profile.tsx
+++ fitness-app-complete/app/(tabs)/profile.tsx
@@ -3,9 +3,24 @@ import { ThemedText } from "@/components/ThemedText";
 import { useThemeColors } from "@/hook/useThemeColors";
 import { Image, StyleSheet, View } from "react-native";
 import { PersonalInfo } from "@/components/Profile/PersonalInfo";
+import { IconList } from "@/components/Profile/ProfileItem";
 
-import ArrowRight from "@/assets/images/icons/arrow-right.svg";
 import { BackButton } from "@/components/UI/BackButton";
+import { ProfileItem } from "@/components/Profile/ProfileItem";
+
+type listType = {
+  icon: keyof typeof IconList;
+  name: string;
+};
+
+const profileItemList: listType[] = [
+  { icon: "profile", name: "Profile" },
+  { icon: "favorite", name: "Favorites" },
+  { icon: "privacy", name: "Privacy Policy" },
+  { icon: "setting", name: "Settings" },
+  { icon: "help", name: "Help" },
+  { icon: "logout", name: "Logout" },
+];
 
 export default function profile() {
   const colors = useThemeColors();
@@ -26,6 +41,11 @@ export default function profile() {
       </View>
       <View style={styles.bottomSection}>
         <PersonalInfo />
+        <View style={styles.profileItems}>
+          {profileItemList.map((item, index) => (
+            <ProfileItem key={index} icon={item.icon} name={item.name} />
+          ))}
+        </View>
       </View>
     </RootView>
   );
@@ -47,5 +67,10 @@ const styles = StyleSheet.create({
   },
   bottomSection: {
     position: "relative",
+  },
+  profileItems: {
+    flexDirection: "column",
+    gap: 20,
+    marginTop: 120,
   },
 });
---
+++ fitness-app-complete/components/Profile/ProfileItem.tsx
@@ -0,0 +1,66 @@
+import { StyleSheet, TouchableOpacity, View } from "react-native";
+import { useThemeColors } from "@/hook/useThemeColors";
+import { ThemedText } from "../ThemedText";
+
+import UserIcon from "@/assets/images/icons/user-white.svg";
+import StarIcon from "@/assets/images/icons/star.svg";
+import PrivacyIcon from "@/assets/images/icons/privacy.svg";
+import SettingIcon from "@/assets/images/icons/setting.svg";
+import SupportIcon from "@/assets/images/icons/support.svg";
+import LogoutIcon from "@/assets/images/icons/log-out.svg";
+import ArrowRight from "@/assets/images/icons/arrow-right.svg";
+
+export const IconList = {
+  profile: UserIcon,
+  favorite: StarIcon,
+  privacy: PrivacyIcon,
+  setting: SettingIcon,
+  help: SupportIcon,
+  logout: LogoutIcon,
+};
+
+type PropsType = {
+  icon: keyof typeof IconList;
+  name: string;
+};
+
+export function ProfileItem({ icon, name }: PropsType) {
+  const colors = useThemeColors();
+  const IconComponent = IconList[icon];
+
+  return (
+    <TouchableOpacity style={styles.container}>
+      <View style={styles.iConTextGroup}>
+        <View
+          style={[styles.iconContainer, { backgroundColor: colors.purple }]}
+        >
+          <IconComponent width={20} height={20} />
+        </View>
+
+        <ThemedText variant="subtitle">{name}</ThemedText>
+      </View>
+
+      <ArrowRight />
+    </TouchableOpacity>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+  iconContainer: {
+    justifyContent: "center",
+    alignItems: "center",
+    height: 40,
+    width: 40,
+    borderRadius: 20,
+  },
+  iConTextGroup: {
+    flexDirection: "row",
+    alignItems: "center",
+    gap: 20,
+  },
+});
